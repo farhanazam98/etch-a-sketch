e4be0ed5211c24bc1a6a7d941647c90146d223d1
Farhan Azam <40482801+farhanazam98@users.noreply.github.com>
2023-04-13 19:39:08 -0700

    Add Barebones Implementation

---
+++ script.js
@@ -0,0 +1,43 @@
+const header = document.querySelector('h1.header');
+const grid = document.querySelector('div.grid');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    var input = Number(prompt("Enter the number of squares per a side: "));
+    if (valid(input)){
+        clearGrid();
+        drawGrid(input);
+    } else{
+        alert("Input was invalid");
+    }
+});
+
+function valid(input){
+    return input !== NaN && input > 0 && input <= 100;
+}
+
+function clearGrid(){
+    const rows = document.querySelectorAll('div.row');
+    rows.forEach(row => {
+        const cells = row.querySelectorAll('div.cell');
+        cells.forEach(cell => {row.removeChild(cell)});
+        grid.removeChild(row);
+    });
+
+}
+
+function drawGrid(squaresPerSide) {
+    for (let i = 0; i < squaresPerSide; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < squaresPerSide; j++){
+            const cell = document.createElement('div');
+            cell.addEventListener("mouseover", (event) => {cell.classList.add('hover')});
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+}
+
+drawGrid(4);
